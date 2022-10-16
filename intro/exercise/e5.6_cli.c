50aae0949c05cc3d296a25692c5530c48fa9e028
mghio <mghio.dev@gmail.com>
2022-10-16 08:53:30 +0800

    chapter 5: exercise 5.6 serv and cli

--- intro/exercise/e5.6_cli.c
+++ intro/exercise/e5.6_cli.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
     sockfd = Socket(AF_INET, SOCK_STREAM, 0);
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(3);
+    servaddr.sin_port = htons(13);
     Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
     Signal(SIGPIPE, sigpipe_handle);
