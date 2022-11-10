9be962ec47041b8dfd1630bd375277461ba081f1
mghio <mghio.dev@gmail.com>
2022-11-10 23:32:12 +0800

    chapter 16: add nonblock_writen_get_cmd.c file

---
+++ example_programs/nonblock_writen_get_cmd.c
@@ -0,0 +1,18 @@
+#include <nonblock_web.h>
+
+void writen_get_cmd(struct file *fptr)
+{
+    int n;
+    char line[MAXLINE];
+
+    n = snprintf(line, sizeof(line), GET_CMD, fptr->f_name);
+    Writen(fptr->f_fd, line, n);
+    printf("wroten %d bytes for %s\n", n, fptr->f_name);
+
+    fptr->f_flags = F_READING;  /* clears F_CONNECTING */
+    FD_SET(fptr->f_fd, &rset);  /* will read server's reply */
+    if (fptr->f_fd > maxfd)
+    {
+        maxfd = fptr->f_fd;
+    }
+}
