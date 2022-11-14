9db5ef864c7ee4642c0191211789aadc0f20f75a
mghio <mghio.dev@gmail.com>
2022-11-14 23:12:52 +0800

    chapter 18: add libroute_rt_iflist.c file

---
+++ example_programs/libroute_net_rt_iflist.c
@@ -0,0 +1,32 @@
+#include <unproute.h>
+
+char * net_rt_iflist(int family, int flags, size_t *lenp)
+{
+    int mib[6];
+    char *buf;
+
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = family;  /* only addresses of this family */
+    mib[4] = NET_RT_IFLIST;
+    mib[5] = flags;   /* interface index or 0 */
+
+    if (sysctl(mib, 6, NULL, lenp, NULL, 0) < 0)
+    {
+        return (NULL);
+    }
+
+    if ( (buf = malloc(*lenp)) == NULL)
+    {
+        return (NULL);
+    }
+
+    if (sysctl(mib, 6, buf, lenp, NULL, 0) < 0)
+    {
+        free(buf);
+        return (NULL);
+    }
+
+    return (buf);
+}
