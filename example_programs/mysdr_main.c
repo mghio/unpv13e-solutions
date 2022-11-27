26271a2fd4208d488bb1239ad929802d908ec8f2
mghio <mghio.dev@gmail.com>
2022-11-27 13:09:38 +0800

    chapter 21: update mysdr_main.c file

--- example_programs/mysdr_main.c
+++ example_programs/mysdr_main.c
@@ -1,4 +1,4 @@
-#include <unp.h>
+#include <mysdr.h>
 
 #define SAP_NAME "sap.mcast.net"  /* default group name and port */
 #define SAP_PORT "9875"
@@ -30,6 +30,54 @@ int main(int argc, char ** argv)
 
     Mcast_join(sockfd, sa, salen, (argc == 4) ? argv[3] : NULL, 0);
     loop(sockfd, salen);  /* receive and print */
-    
+
     exit(0);
+}
+
+void loop(int sockfd, socklen_t salen)
+{
+    socklen_t len;
+    ssize_t n;
+    char *p;
+    struct sockaddr *sa;
+    struct sap_packet {
+        uint32_t sap_header;
+        uint32_t sap_src;
+        char sap_data[BUFFSIZE];
+    } buf;
+    
+    sa = Malloc(salen);
+    for ( ; ; )
+    {
+        len = salen;
+        n = Recvfrom(sockfd, &buf, sizeof(buf) - 1, 0, sa, &len);
+        ((char *) &buf)[n] = 0;
+        buf.sap_header = ntohl(buf.sap_header);
+        printf("From %s hash 0x%04x\n", Sock_ntop(sa, len), buf.sap_header & SAP_HASH_MASK);
+
+        if (((buf.sap_header & SAP_VERSION_MASK) >> SAP_VERSION_SHIFT) > 1)
+        {
+            err_msg("... version field not 1 (0x%08x)", buf.sap_header);
+            continue;
+        }
+
+        if (buf.sap_header & SAP_IPV6)
+        {
+            err_msg("... IPV6");
+            continue;
+        }
+
+        if (buf.sap_header & (SAP_DELETE | SAP_ENCRYPTED | SAP_COMPRESSED))
+        {
+            err_msg("... cant't parse this packet type (0x%08x)", buf.sap_header);
+            continue;
+        }
+
+        p = buf.sap_data + ((buf.sap_header & SAP_AUTHLEN_MASK) >> SAP_AUTHLEN_SHIFT);
+        if (strcmp(p, "application/dsp") == 0)
+        {
+            p += 16;
+        }
+        printf("%s\n", p);
+    }
 }
