2e20b3ae290a250e08dcd977704ce8f57078086b
mghio <mghio.dev@gmail.com>
2023-05-11 22:32:15 +0800

    chapter 24: add sig_io method in dgecho01.c file

--- example_programs/sigio/dgecho01.c
+++ example_programs/sigio/dgecho01.c
@@ -68,3 +68,42 @@ void dg_echo(int sockfd_arg, SA *pcliaddr, socklen_t clilen_arg)
         nqueue--;
     }
 }
+
+static void sig_io(int signo)
+{
+    ssize_t len;
+    int nread;
+    DG *ptr;
+
+    for (nread = 0; ; ) {
+        if (nqueue >= QSIZE)
+        {
+            err_quit("receive overflow");
+        }
+
+        ptr = &dg[iput];
+        ptr->dg_len = clilen;
+        len = recvfrom(sockfd, ptr->dg_data, MAXDG, 0,
+                       ptr->dg_sa, &ptr->dg_salen);
+        if (len < 0)
+        {
+            if (errno == EWOULDBLOCK)
+            {
+                break;  /* all done; no more queued to read */
+            }
+            else 
+            {
+                err_sys("recvfrom error");
+            }
+        }
+
+        ptr->dg_len = len;
+        nread++;
+        nqueue++;
+        if (++iput >= QSIZE)
+        {
+            iput = 0;
+        }
+    }
+    cntread[nread]++;
+}
